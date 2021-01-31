26f6fa557d24d19bcb2d26bc5c7416c9cfb0d8c9
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-01-31 19:08:12 +0300

    First code setup with IBOutlets is done

--- RGBsliders/ViewController.swift
+++ RGBsliders/ViewController.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        coloredView.layer.cornerRadius = 20
+        
+//        redColorLabel
         
     }
 
