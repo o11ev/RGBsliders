3451bf7a8dbb04e6912fd050f79ffb65636c04d8
Olga Dragunova <75809668+o11ev@users.noreply.github.com>
2021-01-31 19:33:33 +0300

    In progress (still working)

--- RGBsliders/ViewController.swift
+++ RGBsliders/ViewController.swift
@@ -8,26 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //view
     @IBOutlet var coloredView: UIView!
     
+    //labels
     @IBOutlet var redColorLabel: UILabel!
     @IBOutlet var greenColorLabel: UILabel!
     @IBOutlet var blueColorLabel: UILabel!
     
-    
+    //sliders
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UILabel!
+    @IBOutlet var blueSlider: UISlider!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         coloredView.layer.cornerRadius = 20
         
-//        redColorLabel
-        
+        let startSliderValue: Float = 0
+        redSlider.value = startSliderValue
+        greenSlider.value = startSliderValue
+        blueSlider.value = startSliderValue
     }
 
+    
+    @IBAction func redSliderAction() {
+        redColorLabel.text = String(format: "%.02f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenColorLabel.text = String(format: "%.02f", greenSlider.value)
+    }
+    
+    
+    @IBAction func blueSliderAction() {
+        blueColorLabel.text = String(format: "%.02f", blueSlider.value)
+    }
+    
 
 }
 
